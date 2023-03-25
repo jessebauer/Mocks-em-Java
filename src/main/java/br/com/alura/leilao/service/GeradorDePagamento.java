0a7180900dfb9ed09ada45f8ff66452a57158952
Jesse Ruan <jesse_ruan@hotmail.com>
2023-03-25 10:32:28 -0300

    Metodo estatico mockito

--- src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
+++ src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -1,22 +1,28 @@
 package br.com.alura.leilao.service;
 
-import java.time.LocalDate;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Pagamento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.LocalDate;
 
 @Service
 public class GeradorDePagamento {
 
-	@Autowired
 	private PagamentoDao pagamentos;
+	private Clock clock;
+
+	@Autowired
+	public GeradorDePagamento(PagamentoDao pagamentos, Clock clock) {
+		this.pagamentos = pagamentos;
+		this.clock = clock;
+	}
 
 	public void gerarPagamento(Lance lanceVencedor) {
-		LocalDate vencimento = LocalDate.now().plusDays(1);
+		LocalDate vencimento = LocalDate.now(clock).plusDays(1);
 		Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
 		this.pagamentos.salvar(pagamento);
 	}
