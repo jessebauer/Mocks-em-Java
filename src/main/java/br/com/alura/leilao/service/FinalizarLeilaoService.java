3de97345a6890b27051e3eaf821b471a03f9d4a1
Jesse Ruan <jesse_ruan@hotmail.com>
2023-03-25 09:23:22 -0300

    Simulando exceptions

--- src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -1,5 +1,6 @@
 package br.com.alura.leilao.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,12 @@ import br.com.alura.leilao.model.Leilao;
 public class FinalizarLeilaoService {
 
 	private LeilaoDao leiloes;
+	private EnviadorDeEmails enviadorDeEmails;
 
 	@Autowired
-	public FinalizarLeilaoService(LeilaoDao leiloes) {
+	public FinalizarLeilaoService(LeilaoDao leiloes, EnviadorDeEmails enviadorDeEmails) {
 		this.leiloes = leiloes;
+		this.enviadorDeEmails = enviadorDeEmails;
 	}
 
 	public void finalizarLeiloesExpirados() {
@@ -26,11 +29,13 @@ public class FinalizarLeilaoService {
 			leilao.setLanceVencedor(maiorLance);
 			leilao.fechar();
 			leiloes.salvar(leilao);
+
+			enviadorDeEmails.enviarEmailVencedorLeilao(maiorLance);
 		});
 	}
 
 	private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
-		List<Lance> lancesDoLeilao = leilao.getLances();
+		List<Lance> lancesDoLeilao = new ArrayList<>(leilao.getLances());
 		lancesDoLeilao.sort((lance1, lance2) -> {
 			return lance2.getValor().compareTo(lance1.getValor());
 		});
